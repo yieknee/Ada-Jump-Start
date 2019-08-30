34a4df88aea7cd92aac14fc947a2bbf89a7a99d7
Yieni <yaynee8d@gmail.com>
2019-08-30 10:55:54 -0400

    Part 1 number assignment

---
+++ numbers_assignment.rb
@@ -0,0 +1,13 @@
+puts "Enter positive integer value."
+var1= gets.to_i
+
+puts "Enter another positive integer value."
+var2= gets.to_i 
+
+puts "Enter final positive interger value."
+var3= gets.to_i
+
+puts "What is twenty added to the numbers you inputed?"
+
+puts "Twenty added to the numbers you inputed are #{var1 +=20}, #{var2+=20} and #{var3+=20}."
+
