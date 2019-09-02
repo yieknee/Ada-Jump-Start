ca018fa247f9ac605c7802db74cb42579b684e45
Yieni <yaynee8d@gmail.com>
2019-09-02 11:21:11 -0400

    Numbers Assignment 2

--- numbers_assignment.rb
+++ numbers_assignment.rb
@@ -1,13 +1,28 @@
 puts "Enter positive integer value."
-var1= gets.to_i
+var1 = gets.chomp.to_i
+
+if var1 => 20
+    puts "#{var1} is greater than or equal to 20!"
+else 
+    puts "#{var1} is less than 20!"
+end
 
 puts "Enter another positive integer value."
-var2= gets.to_i 
+var2 = gets.chomp.to_i 
+
+if var2 => 20
+    puts "#{var2} is greater than or equal to 20!"
+else 
+    puts "#{var2} is less than 20!"
+end
 
 puts "Enter final positive interger value."
-var3= gets.to_i
+var3 = gets.chomp.to_i
 
-puts "What is twenty added to the numbers you inputed?"
+if var3 => 20
+    puts "#{var3} is greater than or equal to 20!"
+else 
+    puts "#{var3} is less than 20!"
+end
 
-puts "Twenty added to the numbers you inputed are #{var1 +=20}, #{var2+=20} and #{var3+=20}."
 
