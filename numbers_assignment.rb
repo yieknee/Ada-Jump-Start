4ed4d5a936901c1871bf953d9092f6a5a0745630
Yieni <yaynee8d@gmail.com>
2019-09-02 12:36:43 -0400

    Fix operator

--- numbers_assignment.rb
+++ numbers_assignment.rb
@@ -1,7 +1,7 @@
 puts "Enter positive integer value."
 var1 = gets.chomp.to_i
 
-if var1 => 20
+if var1 >= 20
     puts "#{var1} is greater than or equal to 20!"
 else 
     puts "#{var1} is less than 20!"
@@ -10,7 +10,7 @@ end
 puts "Enter another positive integer value."
 var2 = gets.chomp.to_i 
 
-if var2 => 20
+if var2 >= 20
     puts "#{var2} is greater than or equal to 20!"
 else 
     puts "#{var2} is less than 20!"
@@ -19,7 +19,7 @@ end
 puts "Enter final positive interger value."
 var3 = gets.chomp.to_i
 
-if var3 => 20
+if var3 >= 20
     puts "#{var3} is greater than or equal to 20!"
 else 
     puts "#{var3} is less than 20!"
