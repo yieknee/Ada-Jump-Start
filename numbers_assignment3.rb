5b57c5b44547affd29fc34c4934d340cd991ccfb
Yieni <yaynee8d@gmail.com>
2019-09-12 00:25:41 -0400

    Number assignment using while

--- numbers_assignment3.rb
+++ numbers_assignment3.rb
@@ -1,12 +1,13 @@
+#get how many times to ask question
 puts "Lets play a numbers game. How many numbers would you like to enter? Postive numbers only :)"
 iteration = gets.chomp.to_i
 
-question = 0 
+question = 0 #start count
 
-until question == iteration
+while question < iteration # keep asking question below until it matches inital response
     puts "Enter your number!"
-    number = gets.chomp.to_i
-    if number % 3 == 0
+    number = gets.chomp.to_i #get number
+    if number % 3 == 0 #if the number is divisible by 3 give response.
         puts "#{number} is divisible by 3!"
     else puts "#{number} is not divisible by 3! There is a remainder of #{number % 3}."
     end
